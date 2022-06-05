50ce53c1bbffc35d9b3371c016f1b449d959ed9a
Gabriel <gabriel.ifsal@gmail.com>
2022-06-05 15:08:16 -0300

    Configuring the code linters (eslint and prettier)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,10 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          { /* fontes externas */ }
-        </Head>
+        <Head>{/* fontes externas */}</Head>
         <body>
           <Main />
           <NextScript />
