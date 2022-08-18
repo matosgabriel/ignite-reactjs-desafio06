74459c364dcacc6ccc6cc5afb7b422b0c5ef37b6
Gabriel <gabriel.ifsal@gmail.com>
2022-08-18 19:46:26 -0300

    Creating header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
+import { Flex, Image } from '@chakra-ui/react';
+
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <Flex as='header' w='100%' h='100px' align='center' justify='center'>
+        <Image src='/Logo.svg' h='46px' />
+      </Flex>
+      {/* <h1>Home</h1> */}
     </>
   );
 }
